35f618a0cca1f796b2ad385c66b27fc600a9d48a
root <root@Zaks-PC>
2024-07-29 20:36:05 +0100

    basic tokenisation

---
+++ lex.py
@@ -0,0 +1,106 @@
+import enum
+import sys
+
+class Lexer:
+    def __init__(self, source):
+        self.source = source + '\n'
+        self.curChar = ''
+        self.curPos = -1
+        self.nextChar()
+
+    # Invalid token found, print error message and exit.
+    def abort(self, message):
+        sys.exit("Lexing error. " + message)
+
+    # Invalid token found, print error message and exit.
+    def abort(self, message):
+        sys.exit("Lexing error. " + message)
+
+    # Process the next character.
+    def nextChar(self):
+        self.curPos += 1
+        if self.curPos >= len(self.source):
+            self.curChar = '\0' # EOF
+        else:
+            self.curChar = self.source[self.curPos]
+
+    # Return the lookahead character.
+    def peek(self):
+        if self.curPos + 1 >= len(self.source):
+            return '\0'
+        return self.source[self.curPos+1]
+
+    # Invalid token found, print error message and exit.
+    def abort(self, message):
+        pass
+
+    # Skip whitespace except newlines, which we will use to indicate the end of a statement.
+    def skipWhitespace(self):
+        while self.curChar == ' ' or self.curChar == '\t' or self.curChar == '\r':
+            self.nextChar()
+
+    # Skip comments in the code.
+    def skipComment(self):
+        pass
+
+    # Return the next token.
+    def getToken(self):
+        self.skipWhitespace()
+        token = None
+
+        if self.curChar == '+':
+            token = Token(self.curChar, TokenType.PLUS)
+        elif self.curChar == '-':
+            token = Token(self.curChar, TokenType.MINUS)
+        elif self.curChar == '*':
+            token = Token(self.curChar, TokenType.ASTERISK)
+        elif self.curChar == '/':
+            token = Token(self.curChar, TokenType.SLASH)
+        elif self.curChar == '\n':
+            token = Token(self.curChar, TokenType.NEWLINE)
+        elif self.curChar == '\0':
+            token = Token('', TokenType.EOF)
+        else:
+            # Unknown token!
+            self.abort("Unknown token: " + self.curChar)
+        
+        self.nextChar()
+        return token
+
+# Token contains the original text and the type of token.
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText   # The token's actual text. Used for identifiers, strings, and numbers.
+        self.kind = tokenKind   # The TokenType that this token is classified as.
+
+# TokenType is our enum for all the types of tokens.
+class TokenType(enum.Enum):
+	EOF = -1
+	NEWLINE = 0
+	NUMBER = 1
+	IDENT = 2
+	STRING = 3
+	# Keywords.
+	LABEL = 101
+	GOTO = 102
+	PRINT = 103
+	INPUT = 104
+	LET = 105
+	IF = 106
+	THEN = 107
+	ENDIF = 108
+	WHILE = 109
+	REPEAT = 110
+	ENDWHILE = 111
+	# Operators.
+	EQ = 201  
+	PLUS = 202
+	MINUS = 203
+	ASTERISK = 204
+	SLASH = 205
+	EQEQ = 206
+	NOTEQ = 207
+	LT = 208
+	LTEQ = 209
+	GT = 210
+	GTEQ = 211
